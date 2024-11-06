35193c60871bf62b44bbc9a16a5e5d9207096742
Koichi Kobayashi <koichi.code@gmail.com>
2024-11-07 01:52:02 +0900

    DataGridにチケット一覧を表示

--- RedmineClient/XmlData/Issues.cs
+++ RedmineClient/XmlData/Issues.cs
@@ -19,71 +19,107 @@
 
     public class Issue
     {
-        [RedmineHeader("ID")]
+        /// <summary>
+        /// ID
+        /// </summary>
         [XmlElement("id")]
         public int Id { get; set; }
 
-        [RedmineHeader("プロジェクト")]
+        /// <summary>
+        /// プロジェクト
+        /// </summary>
         [XmlElement("project")]
         public IssueProject Project { get; set; }
 
-        [RedmineHeader("トラッカー")]
+        /// <summary>
+        /// トラッカー
+        /// </summary>
         [XmlElement("tracker")]
         public Tracker Tracker { get; set; }
 
-        [RedmineHeader("ステータス")]
+        /// <summary>
+        /// ステータス
+        /// </summary>
         [XmlElement("status")]
         public Status Status { get; set; }
 
-        [RedmineHeader("優先度")]
+        /// <summary>
+        /// 優先度
+        /// </summary>
         [XmlElement("priority")]
         public Priority Priority { get; set; }
 
-        [RedmineHeader("作成者")]
+        /// <summary>
+        /// 作成者
+        /// </summary>
         [XmlElement("author")]
         public Author Author { get; set; }
 
-        [RedmineHeader("カテゴリー")]
+        /// <summary>
+        /// カテゴリー
+        /// </summary>
         [XmlElement("category")]
         public Category Category { get; set; }
 
-        [RedmineHeader("題名")]
+        /// <summary>
+        /// 題名
+        /// </summary>
         [XmlElement("subject")]
         public string Subject { get; set; }
 
-        [RedmineHeader("説明")]
+        /// <summary>
+        /// 説明
+        /// </summary>
         [XmlElement("description")]
         public string Description { get; set; }
 
-        [RedmineHeader("開始日")]
+        /// <summary>
+        /// 開始日
+        /// </summary>
         [XmlElement("start_date")]
         public DateTime StartDate { get; set; }
 
-        [RedmineHeader("期日")]
+        /// <summary>
+        /// 期日
+        /// </summary>
         [XmlElement("due_date")]
         public string DueDate { get; set; }
 
-        [RedmineHeader("進捗率")]
+        /// <summary>
+        /// 進捗率
+        /// </summary>
         [XmlElement("done_ratio")]
         public int DoneRatio { get; set; }
 
-        [RedmineHeader("予定工数")]
+        /// <summary>
+        /// 予定工数
+        /// </summary>
         [XmlElement("estimated_hours")]
         public string EstimatedHours { get; set; }
 
+        /// <summary>
+        /// カスタムフィールド
+        /// </summary>
         [XmlArray("custom_fields")]
         [XmlArrayItem("custom_field")]
         public List<IssueCustomField> CustomFields { get; set; }
 
-        [RedmineHeader("作成日時")]
+        /// <summary>
+        /// 作成日時
+        /// </summary>
         [XmlElement("created_on")]
         public DateTime CreatedOn { get; set; }
 
-        [RedmineHeader("更新日時")]
+        /// <summary>
+        /// 更新日時
+        /// </summary>
         [XmlElement("updated_on")]
         public DateTime UpdatedOn { get; set; }
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
     public class IssueProject
     {
         [XmlAttribute("name")]
